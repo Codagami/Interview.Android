9b9647455c4eb262f9349d07319c19aeb5853a33
Raymond Chan <rwmchan@fb.com>
2018-02-08 10:59:40 -0800

    Support rendering basic list after querying Codagami URL

--- CodagamiChallenge/app/src/main/java/com/example/codagami/codagamichallenge/Objects/Person.java
+++ CodagamiChallenge/app/src/main/java/com/example/codagami/codagamichallenge/Objects/Person.java
@@ -3,7 +3,27 @@ package com.example.codagami.codagamichallenge.Objects;
 /**
  * Created by jakedunahee on 2/7/18.
  */
-
 public class Person {
 
+    private final String mFirstName;
+    private final String mLastName;
+    private final String mAge;
+
+    public Person(String firstName, String lastName, String age) {
+        mFirstName = firstName;
+        mLastName = lastName;
+        mAge = age;
+    }
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public String getAge() {
+        return mAge;
+    }
 }
